aed0fa938f4c27a86d5be4ad299413ee9813cb8c
Roshan Srivastava <roshan.srivastava@gmail.com>
2022-07-17 17:05:50 +1000

    issue fixes

--- src/context/OnboardingContext.tsx
+++ src/context/OnboardingContext.tsx
@@ -6,6 +6,8 @@ import { useRouter } from 'next/router'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from 'src/store'
 
+import { loadEmployee } from 'src/store/employee'
+
 // ** Axios
 import axios from 'axios'
 
@@ -28,19 +30,29 @@ const OnboardingProvider = ({ children }: Props) => {
   // ** Hooks
   const router = useRouter()
   const store = useSelector((state: RootState) => state.onboarding)
+  const dispatch = useDispatch<AppDispatch>()
+
+  const checkIfOnboarded = async () => {
+
+  }
 
   useEffect(() => {
     const initOnboarding = async (): Promise<void> => {
       const path = router.asPath
+
       console.log(path)
       if (path !== '/organisation/onboarding') {
         if (!store.onboardingDone) {
           const userData = window.localStorage.getItem('userData')
-          console.log(path)
-          if (userData) {
+          const storedUser = JSON.parse(userData)
+          const employeeId = storedUser.id;
+          const employee = await dispatch(loadEmployee(employeeId));
+          console.log(employee)
+          if (employee.payload.data.profile) {
+
             const user = JSON.parse(userData)
             console.log(user);
-            if (user.userOnboarded === 'false') {
+            if (!employee.payload.data.profile.onboarded) {
               if (user.role === 'admin') {
                 router.push('/organisation/onboarding')
               }
