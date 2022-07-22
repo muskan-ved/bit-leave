efefa7225a7f28f504c843afda82cc55f57cc213
Muskan <muskan.mangoit@gmail.com>
2022-07-22 19:31:22 +0530

    cleanup unwanted data

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -17,7 +17,7 @@ import { AuthValuesType, RegisterParams, ConfirmUserParams, LoginParams, ResendC
 import { CognitoUser, AuthenticationDetails, CognitoUserPool, CognitoUserAttribute } from "amazon-cognito-identity-js"; //
 import { resolve } from 'path'
 import { AppDispatch } from 'src/store'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import {refreshUserState} from 'src/store/user'
 
 //
@@ -242,6 +242,7 @@ const AuthProvider = ({ children }: Props) => {
         })
       },
       onFailure: (err) => {
+       
         if (err) {
           if (errorCallback) errorCallback({ 'Message': err.message })
         }
