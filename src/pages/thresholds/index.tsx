37d516c0476369a7f792abb99b6f690da9d95fdb
Kushal Vaghani <kushalvaghani@Kushal.modem>
2022-08-01 09:56:28 +1000

    thresholds

--- src/pages/thresholds/index.tsx
+++ src/pages/thresholds/index.tsx
@@ -1,5 +1,5 @@
 // ** React Imports
-import { useContext, useEffect, useState } from 'react'
+import {  useEffect, useState } from 'react'
 
 // ** Context
 import { useAuth } from 'src/hooks/useAuth'
@@ -22,22 +22,20 @@ import CircularProgress from '@mui/material/CircularProgress'
 import Divider from '@mui/material/Divider'
 import { AlertBoxOutline, CurrencyUsd, OfficeBuildingOutline } from 'mdi-material-ui'
 
+// ** Import Toaster
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+//  ** Types Imports
+import { organisation } from 'src/types/organisation';
+
 const baseUrl = 'https://api.bitleave.co/organisations/'
-
-type organisationssettings ={
-  thrleavenotification:number,
-  thrleavewarning:number,
-  thrpayoutfrequency:number
-}
-
-type organisation = 
-{
-  id: number,
-  organisationssettings : Array<organisationssettings>
-}
 
 const Thresholds = () => {
   const [data, setData] = useState<organisation | null>(null);
+  const [leaveNotification, setLeaveNotification] = useState<organisation | number>();
+  const [leaveWarning, setLeaveWarning] = useState<organisation | number>();
+  const [maximumPayout, setMaximumPayout] = useState<organisation | number>();
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const { logout } = useAuth()
   const token = localStorage.getItem("accessToken")
@@ -54,9 +52,11 @@ const Thresholds = () => {
         headers: { 'Authorization': `Bearer ${token}` }
       })
       .then(res => {
-        const data = res.data;
-        setData(data.data.organisation);
-        console.log(data);
+        const data = res.data.data.organisation;
+        setLeaveNotification(data.organisationssettings[0].thrleavenotification)
+        setLeaveWarning(data.organisationssettings[0].thrleavewarning)
+        setMaximumPayout(data.organisationssettings[0].thrpayoutfrequency)
+        setData(data)
         setIsLoading(false);
       })
       .catch((reason: AxiosError) => {
@@ -76,26 +76,80 @@ const Thresholds = () => {
     }
   }, []);
 
+  const handleOnChange = (element:any) =>{
+    if(element.target.name === "Excess Leave Notification"){
+      setLeaveNotification(element.target.value)
+    }
+    else if(element.target.name === "Excess Leave Warning"){
+      setLeaveWarning(element.target.value)
+    }
+    else if(element.target.name === "Maximum Payout"){
+      setMaximumPayout(element.target.value)
+    }
+  }
+
+  const handleSubmit = async(e:any) =>{
+    e.preventDefault();
+
+    if(leaveNotification || leaveWarning || maximumPayout){
+    const data = {
+      thrleavenotification: leaveNotification,
+      thrleavewarning: leaveWarning,
+      thrpayoutfrequency: maximumPayout,
+    }
+    setIsLoading(true);
+    await axios({
+      method: "PUT",
+      url: `${baseUrl}thresholds`,
+      data: data,
+      headers: { 'Authorization': `Bearer ${token}` },
+    }).then((res)=>{
+        setIsLoading(false);
+        toast.success("Successfully updated thresholds", {
+          autoClose: 5000,
+          hideProgressBar: false,
+        })
+        fetchData();
+      }).catch((reason: AxiosError)=>{
+      if (reason.response!.status === 401) {
+        logout()
+      } else {
+        setIsLoading(false);
+        toast.error("Failed to updated thresholds", {
+          autoClose: 5000,
+          hideProgressBar: false,
+        })
+        // Handle else
+      }
+    })
+  }else{
+    //else statement
+  }
+
+  }
+
   if (isLoading)
     return (<CircularProgress color="success" />)
 
   if (!isLoading && data) {
     return (
       <Card>
+        <ToastContainer position="top-right" autoClose={false} newestOnTop={false} closeOnClick	rtl={false}	pauseOnFocusLoss draggable />
         <CardHeader title='Excess Leave Thresholds' subheader={<Divider></Divider>} />
         <CardContent>
           <Typography sx={{ mb: 4.5 }}>
             Adjust the excess Leave thresholds
           </Typography>
-          <form onSubmit={e => e.preventDefault()}>
+          <form onSubmit={handleSubmit}>
             <Grid container spacing={5}>
               <Grid item xs={12}>
                 <TextField
                   fullWidth
                   type='number'
                   label='Excess Leave Notification'
+                  name='Excess Leave Notification'
                   defaultValue={
-                    data.organisationssettings ? data.organisationssettings[0].thrleavenotification : ''
+                    leaveNotification
                   }
                   InputProps={{
                     startAdornment: (
@@ -104,6 +158,7 @@ const Thresholds = () => {
                       </InputAdornment>
                     )
                   }}
+                  onChange={(ele) => handleOnChange(ele)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -111,7 +166,8 @@ const Thresholds = () => {
                   fullWidth
                   type='number'
                   label='Excess Leave Warning'
-                  defaultValue={data.organisationssettings && data.organisationssettings[0].thrleavewarning}
+                  name='Excess Leave Warning'
+                  defaultValue={leaveWarning}
                   InputProps={{
                     startAdornment: (
                       <InputAdornment position='start'>
@@ -119,6 +175,7 @@ const Thresholds = () => {
                       </InputAdornment>
                     )
                   }}
+                  onChange={(ele) => handleOnChange(ele)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -126,7 +183,8 @@ const Thresholds = () => {
                   fullWidth
                   type='number'
                   label='Maximum Payout'
-                  defaultValue={data.organisationssettings && data.organisationssettings[0].thrpayoutfrequency}
+                  name='Maximum Payout'
+                  defaultValue={maximumPayout}
                   InputProps={{
                     startAdornment: (
                       <InputAdornment position='start'>
@@ -134,6 +192,7 @@ const Thresholds = () => {
                       </InputAdornment>
                     )
                   }}
+                  onChange={(ele) => handleOnChange(ele)}
                 />
               </Grid>
               <Grid item xs={12}>
